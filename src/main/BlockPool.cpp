8a467be79324bcd7e764c987a05ddf54d3980592
Jesper Öqvist <joqvist@google.com>
2016-06-30 10:15:12 -0700

    Code cleanup

--- src/main/BlockPool.cpp
+++ src/main/BlockPool.cpp
@@ -196,15 +196,6 @@ void BlockPool::Initialize() {
     }
 }
 
-u64 x = 1234567;
-
-unsigned prng() {
-    x ^= x >> 12;
-    x ^= x << 25;
-    x ^= x >> 27;
-    return (x * 2685821657736338717ull) & 0xFFFFFFFF;
-}
-
 TreeNode* search_tree(uintptr_t data) {
     TreeNode* node = root;
     while (node) {
