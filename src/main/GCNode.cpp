bf3f203d5c277c36cc96ba63f42068fe33ceb226
Jesper Öqvist <joqvist@google.com>
2016-07-18 15:07:48 -0700

    Introduce coalesced reference counting
    Added API for coalesced reference counting in GCNode:
    
    * Added GCNode::attachReference()
    * Added GCNode::retargetReference()
    * Added GCNode::detachReference()
    * Added GCNode::applyToCoalescedReferences()
    * Added GCNode::makeDirt()
    * Added GCNode::s_dirty_nodes
    * Added GCNode::s_remembered_references
    
    One bit has been stolen from the reference count and is used as the
    dirty bit.
    
    The following references are currently coalesced:
    
    * RObject::m_attrib
    * ArgMatcher::m_formals

--- src/main/GCNode.cpp
+++ src/main/GCNode.cpp
@@ -51,9 +51,35 @@
 using namespace std;
 using namespace rho;
 
+// Dirty objects.
+vector<const GCNode*> GCNode::s_dirty_nodes;
+
+// Remembered outgoing references of dirty objects.
+vector<const GCNode*> GCNode::s_remembered_references;
+
 vector<const GCNode*>* GCNode::s_moribund = 0;
 unsigned int GCNode::s_num_nodes = 0;
 bool GCNode::s_on_stack_bits_correct = false;
+
+bool GCNode::isDirty() const {
+    return (m_rcmms & s_dirty_mask) != 0;
+}
+
+void GCNode::clearDirtyFlag() const {
+    m_rcmms &= static_cast<unsigned char>(~s_dirty_mask);
+}
+
+void GCNode::makeDirty() const {
+    if (!isDirty()) {
+        m_rcmms |= s_dirty_mask;
+        s_dirty_nodes.push_back(this);
+        applyToCoalescedReferences([](const GCNode* node) {
+            if (node) {
+                s_remembered_references.push_back(node);
+            }
+        });
+    }
+}
 
 // Used to update reference count bits of a GCNode. The array element at index
 // n+1 is XORed with the current rcmms bits to compute the updated reference
@@ -61,9 +87,7 @@ bool GCNode::s_on_stack_bits_correct = false;
 // the other bits in m_rcmms.
 const unsigned char GCNode::s_decinc_refcount[]
 = {0,    2, 2, 6, 6, 2, 2, 0xe, 0xe, 2, 2, 6, 6, 2, 2, 0x1e,
-   0x1e, 2, 2, 6, 6, 2, 2, 0xe, 0xe, 2, 2, 6, 6, 2, 2, 0x3e,
-   0x3e, 2, 2, 6, 6, 2, 2, 0xe, 0xe, 2, 2, 6, 6, 2, 2, 0x1e,
-   0x1e, 2, 2, 6, 6, 2, 2, 0xe, 0xe, 2, 2, 6, 6, 2, 0,    0};
+   0x1e, 2, 2, 6, 6, 2, 2, 0xe, 0xe, 2, 2, 6, 6, 2, 0, 0};
 
 unsigned char GCNode::s_mark = 0;
 
@@ -85,7 +109,14 @@ HOT_FUNCTION void* GCNode::operator new(size_t bytes) {
     return result;
 }
 
-GCNode::GCNode(CreateAMinimallyInitializedGCNode*) : m_rcmms(s_decinc_refcount[1]) {
+// A minimally initialized GCNode is marked as dirty in order to avoid
+// remembering partially initialized references, just in case any reference
+// mutation method were called on the node before its constructor is called,
+// which normally should not happen.  The real GCNode constructor will insert
+// this node in the dirty list so that its outgoing references are counted
+// before the next GC pass.
+GCNode::GCNode(CreateAMinimallyInitializedGCNode*) :
+    m_rcmms(s_decinc_refcount[1] | s_dirty_mask) {
 }
 
 void GCNode::operator delete(void* pointer, size_t bytes) {
@@ -116,7 +147,30 @@ void GCNode::destruct_aux() {
     }
     s_moribund->erase(it);
 }
-    
+
+void GCNode::applyCoalescedReferenceUpdates() {
+    // Increment all current outgoing references.
+    // This happens first, so that we don't prematurely reach a zero count and
+    // delete a remembered reference.
+    for (auto node : s_dirty_nodes) {
+        node->clearDirtyFlag();
+        node->applyToCoalescedReferences([](const GCNode* node) {
+            incRefCount(node);
+        });
+    }
+    s_dirty_nodes.clear();
+
+    // Decrement the reference count of all remembered references.
+    for (auto node : s_remembered_references) {
+        // Ensure it is still allocated using asGCNode().
+        if (!asGCNode(const_cast<GCNode*>(node))) {
+            Rf_error("remembered reference deleted when expected to not be");
+        }
+        decRefCount(node);
+    }
+    s_remembered_references.clear();
+}
+
 extern RObject* R_Srcref;
 
 void GCNode::gc(bool markSweep) {
@@ -143,6 +197,8 @@ void GCNode::markSweepGC() {
     // any code that depends on normal operation of the garbage collector.
     s_on_stack_bits_correct = true;
 
+    applyCoalescedReferenceUpdates();
+
     mark();
     sweep();
 
@@ -151,6 +207,8 @@ void GCNode::markSweepGC() {
 
 void GCNode::gclite() {
     s_on_stack_bits_correct = true;
+
+    applyCoalescedReferenceUpdates();
 
     while (!s_moribund->empty()) {
         // Last in, first out, for cache efficiency:
